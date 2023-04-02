6ac68758c0825f9c5896638253d49f0ee7214d1c
MOHAMMED-SABITH-C <128989395+MOHAMMED-SABITH-C@users.noreply.github.com>
2023-04-02 19:31:12 +0700

    Update db_functions.dart

--- lib/db/functions/db_functions.dart
+++ lib/db/functions/db_functions.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-//import 'package:flutter_application_2/seconddb/db_functions2.dart';
 import 'package:sqflite/sqflite.dart';
 import '../../model/data_model.dart';
 
@@ -12,80 +11,29 @@ late Database _db;
 
 
 Future<void > InitilizeDataBase1() async{
- //_db = await openDatabase('student.db',      //id INTEGER ,
-  // final dbpath = await sql.getDatabasePath();
-  // return sql.openDatabase(path.join(dbpath,'objects0.db'),onCreate: (db,version) => _createDb(db),version: 1)
+ 
    _db =await openDatabase('objects01.db',
   version: 1,
   onCreate:(Database db, int version) async{
-    //Batch batch = db.batch();
-    ///batch
     db.execute('CREATE TABLE objects01 ( name TEXT PRIMARY KEY , price INTEGER , type TEXT , count TEXT, duration TEXT,imag TEXT)');
-   // batch.execute('CREATE TABLE objects02 ( name TEXT PRIMARY KEY , price TEXT , type TEXT , count TEXT, duration TEXT');
-  // db.execute('CREATE TABLE objects02 ( name TEXT PRIMARY KEY , price TEXT , type TEXT , count TEXT, duration TEXT)');
   } ,
    );
    getAllStudents();
 }
 
-// Future<void > InitilizeDataBase3() async{
-//  //_db = await openDatabase('student.db',      //id INTEGER ,
-//   // final dbpath = await sql.getDatabasePath();
-//   // return sql.openDatabase(path.join(dbpath,'objects0.db'),onCreate: (db,version) => _createDb(db),version: 1)
-//    _db3 =await openDatabase('objects03.db',
-//   version: 1,
-//   onCreate:(Database db, int version) async{
-//     //Batch batch = db.batch();
-//     ///batch
-//     db.execute('CREATE TABLE objects03 ( name TEXT PRIMARY KEY , price TEXT , type TEXT , count TEXT, duration TEXT)');
-//    // batch.execute('CREATE TABLE objects02 ( name TEXT PRIMARY KEY , price TEXT , type TEXT , count TEXT, duration TEXT');
-//   // db.execute('CREATE TABLE objects02 ( name TEXT PRIMARY KEY , price TEXT , type TEXT , count TEXT, duration TEXT)');
-//   } ,
-//    );
-//    getAllStudents3();
-// }
+
 
 Future<void> addStudent(StudentModel value) async{
-//  final studentDB = await Hive.openBox<StudentModel>('students_db');
-//   await studentDB.add(value);
-////if(type == 'Electronic Devices'){
  await _db.rawInsert('INSERT INTO objects01 (name,price,type,count,duration,imag) VALUES (?,?,?,?,?,?)',[value.name,value.price,value.type,value.count,value.duration,value.image]);
 
 getAllStudents();
-//}else if(type == 'Fruits')
-//{
-//   print('Fruits');
-//   await _db3.rawInsert('INSERT INTO objects03 (name,price,type,count,duration) VALUES (?,?,?,?,?)',[value.name,value.price,value.type,value.count,value.duration]);
 
-// getAllStudents3();
-// }
-
-  // StudnetModelNotifier.value.add(value);
-  // StudnetModelNotifier.notifyListeners();
 
 }
 
-// Future<void> getAllStudents3() async{
-//   // final studentDB = await Hive.openBox<StudentModel>('students_db');
-//   // StudnetModelNotifier.value.clear();
-  
-//  final _values = await _db3.rawQuery('SELECT * FROM objects03',);
-//  print(_values);
-// StudnetModelNotifier3.value.clear();
 
-// _values.forEach((map) {
-//   final objects= StudentModel.fromMap(map);
-//   StudnetModelNotifier3.value.add(objects);
-
-//   StudnetModelNotifier3.notifyListeners();
-//  });
-
-// //  StudnetModelNotifier.value.addAll(_values);  //(studentDB.values);
-// }
 
 Future<void> getAllStudents() async{
-  // final studentDB = await Hive.openBox<StudentModel>('students_db');
-  // StudnetModelNotifier.value.clear();
   
  final _values = await _db.rawQuery('SELECT * FROM objects01',);
  print(_values);
@@ -107,14 +55,10 @@ _values.forEach((map) {
  });
 
 
-//  StudnetModelNotifier.value.addAll(_values);  //(studentDB.values);
 }
-//int
 
 Future<void> deleteStudent(String name) async{
   print(name);
- // id='';
-//await deleteDatabase('objects0.db');
   await _db.rawDelete('DELETE FROM objects01 WHERE name=?',[name]);
   getAllStudents();
 }
@@ -137,7 +81,6 @@ Future<void >SearchDb(String col,String name) async{
 
 _value.forEach((map1) {
   final objects= StudentModel1S.fromMap(map1);
- // print(objects);
   StudnetModelNotifier1S.value.add(objects);
 
 
@@ -151,7 +94,6 @@ _value.forEach((map1) {
 
 _value.forEach((map) {
   final objects1= StudentModel1S.fromMap(map);
-  //print(objects1);
   StudnetModelNotifier1S.value.add(objects1);
 
 
@@ -181,19 +123,14 @@ _values.forEach((map) {
 Future<void>sort(String srt,String obj) async{
   obj='price';
   print (srt);
-  //final _values;
   if(srt =='ASC'){
-    // if(obj == 'name')  
-    //   String name='name';
-    // if(obj == 'price')
-    //   String name = 'price';
+   
   final _values = await _db.rawQuery('Select * from objects01 ORDER BY $obj ASC');
   print(_values);
   StudnetModelNotifier1S.value.clear();
 
 _values.forEach((map) {
   final objects1= StudentModel1S.fromMap(map);
- //print(objects1);
   StudnetModelNotifier1S.value.add(objects1);
 
 
@@ -207,7 +144,6 @@ _values.forEach((map) {
 
 _values.forEach((map) {
   final objects1= StudentModel1S.fromMap(map);
- // print(objects1);
   StudnetModelNotifier1S.value.add(objects1);
 
 
@@ -215,31 +151,19 @@ _values.forEach((map) {
     });
   }
 
- // getAllStudents2();
 }
 
 
-//display product 0details
 
 Future<void> ItemDetails(String name) async{
- //var table;
   print('$name');
   final _values = await _db.rawQuery('Select * from objects01 where name like ? ',['$name',]);
- // table = _values;
-
- // print(table);
- // return table;
-  //return ItemDetails();
 
    StudnetModelNotifier1S.value.clear();
-
-//table.
 _values.forEach((map) {
   final objects1= StudentModel1S.fromMap(map);
- // print(objects1);
    StudnetModelNotifier1S.value.add(objects1);
 
-    //;
    StudnetModelNotifier1S.notifyListeners();
     });
     
