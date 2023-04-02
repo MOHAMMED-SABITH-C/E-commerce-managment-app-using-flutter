29f5b6ceaf05aaebba78ab4b64a4a1f6c5aa2048
MOHAMMED-SABITH-C <128989395+MOHAMMED-SABITH-C@users.noreply.github.com>
2023-04-02 19:47:19 +0700

    Update printcartBill.dart

--- lib/Users/cart/printcartBill.dart
+++ lib/Users/cart/printcartBill.dart
@@ -15,7 +15,6 @@ import '../../4rthdb/db_function4.dart';
 
 class printCartBill extends StatefulWidget {
    printCartBill({super.key,required this.time,required this.Address});
-  //  getD();
     late int time;
     late String Address='';
     late int totalPrice =0;
@@ -39,36 +38,13 @@ class _printCartBillState extends State<printCartBill> {
         
      widget.totalPrice=total;
       });
-    //return Scaffold(
-
-    
-    
-    
-    
-//     );
-//   }
-// }
-// import 'package:flutter/material.dart';
-// import 'package:flutter/src/widgets/container.dart';
-// import 'package:flutter/src/widgets/framework.dart';
-// import 'package:flutter_application_2/buttenNavig.dart';
-
-// class PrintCartBill extends StatelessWidget {
-//    PrintBill({super.key,required this.type,required this.image,required this.phone,required this.objName,required this.price,required this.count,required this.list});
-    // late var list;
+ 
     late String image;
-   // late var list2;
     late String phone;
     late String objName;
     late String price;
 
     late String count;
-
-  // @override
-  // Widget build(BuildContext context) {
-   // late var dataU = widget.list2;
-    // late var dataU = list;
-
     print('getout');
     return Scaffold(
       appBar: AppBar(title: Text('BILL'), actions: [IconButton(onPressed: (){
@@ -118,24 +94,7 @@ class _printCartBillState extends State<printCartBill> {
                     SizedBox(height: 10,),
                 ]),
               ),
-              // Container(
-              //   width: 196.3,
-              //   height: 200,
-              //   decoration: BoxDecoration(
-              //   image: DecorationImage(
-              //     image: AssetImage("$image"),
-                
-              //   ),
-                                
-              //  color: Color.fromARGB(83, 145, 193, 200),
-              //     border: Border.all(color: Colors.black),
-              //    borderRadius: BorderRadius.circular(5)
-              //                     ),
-              //   ),
-                // child: Column(children: [
-                //   //Text('hi')
-                // ]),
-            //  )
+             
               
             ],),
             SizedBox(height: 20,),
@@ -150,17 +109,11 @@ class _printCartBillState extends State<printCartBill> {
                                   
                                   itemBuilder: (context, index) {
                                     final data = studentList[index];
-                                    // table = data.table;
                                     image = data.image;
-                                  //  limit = int.parse(data.lim);
-                                    //ogprice =data.ogPrice;
                                     price = data.price;
-                                    // setState(() {
                                       
                                     total= total+ int.parse(price);
-                                    // widget.totalPrice=widget.totalPrice+int.parse(price);
                                     print(widget.totalPrice);
-                                    // });
                                   int  _itemCount= int.parse(data.count);
                                   count =(int.parse(data.lim)-int.parse(data.count)).toString();
                                     deleteCart1(data.objName,data.phone);
@@ -169,28 +122,21 @@ class _printCartBillState extends State<printCartBill> {
                                 print('update');
 
                       updateDb1C(count, data.objName);
-                      // updateDbC('',data.count,data.price, data.objName, widget.phone1,'buy');
                       }
                       else if(data.table =='Vegitables'){
                       updateDb2C(count, data.objName);
-                      // updateDbC('',data.count,data.price, data.objName, widget.phone1,'buy');
 
                       }
                       else if(data.table == 'Fruits'){
                         updateDb3C(count, data.objName);
-                      // updateDbC('',data.count,data.price, data.objName, widget.phone1,'buy');
                       }
                       else if(data.table =='Shirts'){
                         updateDb4C(count, data.objName);
-                      // updateDbC('',data.count,data.price, data.objName, widget.phone1,'buy');
                       }
-                                 // print('$count c $price p $_itemCount i');
                                    return ListTile(
                                       title: Text(data.objName),
                                       isThreeLine: true,
-                                      //Text(data.price),
                                  
-                                     // leading: CircleAvatar(radius: 30),
                                      leading: ConstrainedBox(
                                   constraints: BoxConstraints(
                                     
@@ -201,88 +147,16 @@ class _printCartBillState extends State<printCartBill> {
                                     child: Image.asset("$image"),
                                 
                                   ),
-              
-            //SizedBox(height: 20,),
-           // Text(data.objName,style:TextStyle(fontWeight: FontWeight.bold,)),
-
-            //SizedBox(height: 10,),
             
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-            //   children: [
-                
-            //   // Column(
-            //   //   children: [
-            //       Align(
-            //         alignment: Alignment.centerLeft,
-            //         child:
                     subtitle:Text( '('+data.count+' items)', style: TextStyle(
                                                   color: Color.fromARGB(100, 15, 0, 0),
                                                   fontWeight: FontWeight.bold),),
-                  // ),
-            //       Align(
-            //         alignment: Alignment.centerRight,
-                    // child:
+                
             trailing:
                     Text('MRP:\n₹'+price, style: TextStyle(
                                                   fontSize: 20,color: Color.fromARGB(100, 15, 0, 0),
                                                   fontWeight: FontWeight.bold),),
-            //       ),
-            //   //   ],
-            //   // )
-            //   ],
-            // ),
-            //SizedBox(height: 15,),
-
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-            //   children: [
-                
-            //   // Column(
-            //   //   children: [
-            //       Align(
-            //         alignment: Alignment.centerLeft,
-            //         child:Text('Product Discount', style: TextStyle(
-            //                                       fontSize: 20,color: Color.fromARGB(100, 15, 0, 0),
-            //                                       fontWeight: FontWeight.bold),),
-            //       ),
-            //       Align(
-            //         alignment: Alignment.centerRight,
-            //         child:Text('-₹0', style: TextStyle(
-            //                                       fontSize: 20,color: Color.fromARGB(100, 15, 0, 0),
-            //                                       fontWeight: FontWeight.bold),),
-            //       ),
-            //   //   ],
-            //   // )
-            //   ],
-            // ),
-            // SizedBox(height: 15,),
-
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //   children: [
-                
-            //   // Column(
-            //   //   children: [
-            //       Align(
-            //         alignment: Alignment.centerLeft,
-            //         child:Text('Delivery Fee', style: TextStyle(
-            //                                       fontSize: 20,color: Color.fromARGB(100, 15, 0, 0),
-            //                                       fontWeight: FontWeight.bold),),
-            //       ),
-            //       Align(
-            //         alignment: Alignment.centerRight,
-            //         child:Text('₹50', style: TextStyle(
-            //                                       fontSize: 20,color: Color.fromARGB(100, 15, 0, 0),
-            //                                       fontWeight: FontWeight.bold),),
-            //       ),
-            //   //   ],
-            //   // )
-            //   ],
-            // ),
-            // SizedBox(height: 15,),
+           
              );
                                   },
                                   separatorBuilder: (context, index) {
@@ -316,8 +190,7 @@ class _printCartBillState extends State<printCartBill> {
                                                   fontSize: 30,color: Color.fromARGB(255, 15, 0, 0),
                                                   fontWeight: FontWeight.bold),),
                   ),
-              //   ],
-              // )
+            
               ],
             ),
             // Divider(thickness: 5,color: Color.fromARGB(206, 60, 59, 59),),
@@ -333,18 +206,10 @@ class _printCartBillState extends State<printCartBill> {
     final SharedPreferences sp = await _pref;
 
     setState(() {
-    // final 
-    //  ph 
-      // = ph;
       phone0= sp.getString('phone')??'';
      widget.phone1=phone0;
-      //final
-      //  na
       widget.name1= sp.getString('name')??'';
       widget.totalPrice;
-        // =na; 
-
-      //print(price+'hm');
     });
     getcart(phone0);
 
